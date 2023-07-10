28a617bc1897ac850ddd3e16beaf0354ce2eeed9
Naveen Kumar <naveen.umar@jalantechnologies.com>
2023-07-10 22:07:26 +0530

    added Remove Duplicates From Sorted Array solution

---
+++ Remove Duplicates From Sorted Array/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() < 2) {
+            return nums.size();
+        }
+        int i = 0, j = 1;
+        while(j < nums.size()) {
+            if(nums[i] == nums[j])
+                j++;
+            else {
+                i++;
+                nums[i] = nums[j];
+                j++;
+            }
+        }
+        return i+1;
+    }
+};
