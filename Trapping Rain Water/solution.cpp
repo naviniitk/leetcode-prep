80ae072c3ac4a462be232752634c5b21914148e9
Naveen Kumar <navin10102001@gmail.com>
2023-07-22 11:56:51 +0530

    added better solution for Trapping Rain Water

--- Trapping Rain Water/solution.cpp
+++ Trapping Rain Water/solution.cpp
@@ -19,3 +19,42 @@ public:
         return ans;
     }
 };
+
+// O(n) time and O(1) space
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int lmax = height[0];
+        int rmax = height[n-1];
+        int lpos = 1;
+        int rpos = n-2;
+        int water = 0;
+        while(lpos <= rpos)
+        {
+            if(height[lpos] >= lmax)
+            {
+                lmax = height[lpos];
+                lpos++;
+            }
+            else if(height[rpos] >= rmax)
+            {
+                rmax = height[rpos];
+                rpos--;
+            }
+            else if(lmax <= rmax)
+            {
+                water += lmax - height[lpos];
+                lpos++;
+            }
+            else
+            {
+                water += rmax - height[rpos];
+                rpos--;
+            }
+        
+        }
+        return water;
+    }
+};
