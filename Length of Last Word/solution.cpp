63de6f3625db9bf14d16bc3b8496b5851de07c95
Naveen Kumar <navin10102001@gmail.com>
2023-07-22 12:07:15 +0530

    added solution for Length of Last Word

---
+++ Length of Last Word/solution.cpp
@@ -0,0 +1,19 @@
+// O(n) time O(1) space
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size();
+        int ans = 0;
+        for(int j = n-1; j >= 0; j--) {
+            if(ans > 0 && s[j] == ' ') break;
+            while(s[j] == ' ' && ans == 0) {
+                j--;
+                continue;
+            }
+            if(s[j] != ' ') {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
